9fe0160405bf6b6522e1ee22500fe4877699822b
oksana-rybalka <rybalka-oksana@outlook.com>
2024-10-04 14:27:48 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const submitForm = document.querySelector('login-form');
+submitForm.addEventListener('submit', handleSubmit());
+function handleSubmit(event) {
+    event.preventDefault();
+    const inputEmailValue = event.target.elements.email.value.trim();
+    const inputPasswordValue = event.target.elements.password.value.trim();
+    if( inputEmailValue === '' || inputPasswordValue === '') {
+        alert('All form fields must be filled in');
+        return
+    } 
+    const info = {
+            email:inputEmailValue,
+            password:inputPasswordValue,
+        }
+        console.log(info);
+        submitForm.reset();
+    };
 
