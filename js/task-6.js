acf6916da1543ed011d4b6492701976e5adc8304
oksana-rybalka <rybalka-oksana@outlook.com>
2024-10-07 15:30:44 +0300

    troshechky

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+let sizes = 30;
+
+btnCreate.addEventListener("click", () => {
+   if (input.value < 1 || input.value > 100) {
+    return;
+  }
+  createBoxes(input.value);
+});
+
+btnDestroy.addEventListener("click", () => {
+  input.value = "";
+  cleanClick()
+});
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${sizes}px`;
+    box.style.height = `${sizes}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+    sizes += 10;
+  }
+
+}
+function cleanClick() {
+  boxes.innerHTML = "";
+  sizes = 30;
+}
